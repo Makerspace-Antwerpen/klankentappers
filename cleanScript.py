d6ddf700e4c59dbaa21962c3c88232161c6f96af
Wouter Peetermans <wouter@peetermans.dev>
2021-02-05 15:13:02 +0000

    db measurement sort of working

--- cleanScript.py
+++ cleanScript.py
@@ -12,16 +12,53 @@
 import fileinput
 import math
 
+class DFTPair:
+    def __init__(self, freq, amp):
+        self.freq = freq
+        self.amp = amp
 
 
-def freqBinToOct(freqBin):
+def calcRa(freq):
+    ra = (148693636 * freq**4)/((freq**2 + 424.36) * math.sqrt((freq**2 + 11599.29)*(freq**2 + 544496.41)) * (freq**2 + 148693636))
+    return ra;
+
+def dbaWeight(freq):
+    ra = calcRa(freq)
+    a = (20 * math.log(ra,10)) + 2 #- ( 20 * math.log(calcRa(1000),10))
+    return a;
+    
+def calcDb(amp):
+    if amp == 0:
+        return 0
+    db = 20 * math.log(amp/0.00002,10)
+    return db
+
+
+def freqBinToOct(freqBin): #needs mod to take in amplitude correction.
     total = 0
     for freq in freqBin:
         freq *= 100000
         total += (freq**2)
     power = math.sqrt( total )
-    db = 20 * math.log(power/20000000)
+    db = 20 * math.log(power/20000000,10)
     return db
+
+def calcDBaFromDFT(dft):
+    subtotal = 0
+    for dftPair in dft:
+        if dftPair.freq == 0:
+            continue
+        db = calcDb(dftPair.amp)
+        weight = dbaWeight(dftPair.freq)
+        #print("freq: " + str(dftPair.freq) + " weight: " + str(weight))
+        weightedDb = db + weight
+        if weightedDb < 0:
+            weightedDb = 0
+        subtotal += (weightedDb/10)**10
+    if subtotal == 0: # it's ugly I know. 
+        subtotal = 1
+    #print("subtotal: " + str(subtotal))
+    return math.log(subtotal,10)*10;
 
 freqList = [ 11.2 , 14.1 , 17.8 , 22.4 , 28.2 , 35.5 , 44.7 , 56.2 , 70.8 , 89.1 , 112 , 141 , 178 ,
         224 , 282 , 355 , 447 , 562 , 708 , 891 , 1122 , 1413 , 1778 , 2239 , 2818 , 3548 ,
@@ -29,6 +66,8 @@ freqList = [ 11.2 , 14.1 , 17.8 , 22.4 , 28.2 , 35.5 , 44.7 , 56.2 , 70.8 , 89.1
 stepCount = 0;
 nextOctaveList = list()
 nextFreqBin = list()
+nextDFT = list()
+
 
 with fileinput.input() as f_input:
     for line in f_input:
@@ -36,11 +75,12 @@ with fileinput.input() as f_input:
         if dataEntry[0] != "":
             if len(dataEntry) == 3:
                 freq = float(dataEntry[0])
-                val = float(dataEntry[2])
+                amp = float(dataEntry[2])
                 #print(freq)
+                nextDFT.append(DFTPair(freq, amp))
                 
                 if freq < freqList[stepCount]:
-                    nextFreqBin.append(val);
+                    nextFreqBin.append(amp);
                 else:
                     #print(nextFreqBin)
                     nextOctaveList.append(freqBinToOct(nextFreqBin))
@@ -48,7 +88,10 @@ with fileinput.input() as f_input:
                     stepCount += 1
 
                 if stepCount > 33 or freq == 0:
-                    print(nextOctaveList)
+                    db = calcDBaFromDFT(nextDFT)
+                    print(db)
+                    nextDFT.clear()
+                    #print(nextOctaveList)
                     nextOctaveList.clear()
                     stepCount = 0
                 #print(len(dataEntry))
