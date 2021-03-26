5584bd14e42b772a9ddeaa7283d1188a9040fd9f
Wouter Peetermans <wouter@wouterpeetermans.com>
2021-03-26 17:44:29 +0100

    config support added in recordEvents

--- src/recordEvents.py
+++ src/recordEvents.py
@@ -6,17 +6,26 @@ import datetime
 import soundfile as sf
 import sounddevice as sd
 import numpy as np
+import configparser
 from lib.dbaMeasure import DBAMeasure
 from lib.movingAverage import MovingAverage
 from micSetup.adaI2S import micSetup
 
-MIC_REF_RMS = 0.008324243692980756
-MIC_REF_DBA = 71.5
+config = configparser.ConfigParser()
+config.read('klankConfig.ini')
+micConfig = config['micConfig']
+
+
+
+MIC_REF_RMS = float(micConfig['rmsRefLevel'])
+MIC_REF_DBA = float(micConfig['dbRefLevel'])
+
+
 
 
 mic = micSetup()
 dbaMeasure = DBAMeasure(MIC_REF_RMS, MIC_REF_DBA)
-dbaMA = MovingAverage(8 * 18000, 40)
+dbaMA = MovingAverage(8 * 18000, 55)
 
 audioQueue = queue.Queue()
 dbaQueue = queue.Queue()
@@ -24,7 +33,7 @@ dbaQueue = queue.Queue()
 def audioRecordingCallback(indata):
     audioQueue.put(indata.copy())
     # TODO move all loose values to constants
-    if audioQueue.qsize() > ( 8 * 5 ): # only keep the last 5 seconds
+    if audioQueue.qsize() > ( 8 * 1 ): # only keep the last 5 seconds
         audioQueue.get()
 
 
@@ -49,16 +58,16 @@ def upDateDBaMovingAverage():
 
 while True:
     dba = upDateDBaMovingAverage()
-    #print(str(dba) + "  " + str(dbaMA.getMA()))
+    print(str(dba) + "  " + str(dbaMA.getMA()))
     if dba > dbaMA.getMA() + 10:
         print("event " + str(fileCounter) +" fired")
         lastTime = time.time()
-        fileName = datetime.datetime.now().replace(microsecond=0).isoformat() + ".wav"
+        fileName = "/mnt/harddisk" datetime.datetime.now().replace(microsecond=0).isoformat() + ".wav"
         fileCounter += 1
         with sf.SoundFile(fileName, mode='w', samplerate=48000, format="WAV",
                 channels=1, subtype="PCM_16") as file:
             currentTime = lastTime
-            while (currentTime - 5) < lastTime:
+            while (currentTime - 1) < lastTime:
                 if dbaQueue.empty() == False:
                     if upDateDBaMovingAverage() > dbaMA.getMA() + 10:
                         lastTime = time.time()
