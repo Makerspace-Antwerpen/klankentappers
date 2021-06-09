5fe527d2fc6e90e7336a678ae3f3485d44716cf5
Wouter Peetermans <wouter@wouterpeetermans.com>
2021-06-09 17:29:11 +0200

    modifications made to auto build kernel module

---
+++ src/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OS_VERSION=$(echo "$BALENA_HOST_OS_VERSION" | cut -d " " -f 2)
+echo "OS Version is $OS_VERSION"
+
+cd output
+mod_dir="i2s_mic_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
+for each in $mod_dir; do
+	echo Loading module from "$each"
+	insmod "$each/snd-i2smic-rpi.ko"
+	lsmod | grep sndi2smic-rpi
+	rmmod sndi2smic-rpi
+done
+
+while true; do
+	sleep 60
+done
+
