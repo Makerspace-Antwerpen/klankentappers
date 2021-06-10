ecebe6ca016ef66b71ba7f6d44f53f2ec8fd94fe
Wouter Peetermans <wouter@wouterpeetermans.com>
2021-06-10 12:03:09 +0200

    i2s mic working inside balena

--- src/run.sh
+++ src/run.sh
@@ -7,9 +7,9 @@ cd output
 mod_dir="i2s_mic_module_${BALENA_DEVICE_TYPE}_${OS_VERSION}*"
 for each in $mod_dir; do
 	echo Loading module from "$each"
-	insmod "$each/snd-i2smic-rpi.ko"
+	insmod "$each/snd-i2smic-rpi.ko" rpi_platform_generation=2
 	lsmod | grep sndi2smic-rpi
-	rmmod sndi2smic-rpi
+	#rmmod sndi2smic-rpi
 done
 
 while true; do
